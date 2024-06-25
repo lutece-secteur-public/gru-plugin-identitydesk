306472f3bef8fe9d9d9c2e42c45649abfb069447
seboo <25958061+seboo@users.noreply.github.com>
2024-06-25 18:07:36 +0200

    LUT-28211 : lower plugin rights level

--- src/sql/plugins/identitydesk/core/init_core_identitydesk.sql
+++ src/sql/plugins/identitydesk/core/init_core_identitydesk.sql
@@ -4,7 +4,7 @@
 --
 DELETE FROM core_admin_right WHERE id_right = 'IDENTITYDESK_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES 
-('IDENTITYDESK_MANAGEMENT','identitydesk.adminFeature.ManageIdentities.name',1,'jsp/admin/plugins/identitydesk/ManageIdentities.jsp','identitydesk.adminFeature.ManageIdentities.description',0,'identitydesk',NULL,NULL,NULL,4);
+('IDENTITYDESK_MANAGEMENT','identitydesk.adminFeature.ManageIdentities.name',3,'jsp/admin/plugins/identitydesk/ManageIdentities.jsp','identitydesk.adminFeature.ManageIdentities.description',0,'identitydesk',NULL,NULL,NULL,4);
 
 
 --
